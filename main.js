48b4ee6ef2810f698e0362040a13e831131454cd
andreacapntd <andreacapntd@gmail.com>
2020-07-03 14:46:45 +0200

    ex js jq carousel primo commit

---
+++ main.js
@@ -0,0 +1,5 @@
+// Creare uno slider di immagini
+// Potete usare le immagini che desiderate.
+// Per facilitarvi la vita usate immagini delle stesse dimensioni :slightly_smiling_face:
+// Lo slider prevederà due frecce (icone) che permettono di mostrare le immagini, arrivati all’ultima o prima immagine dobbiamo ripartire dal lato opposto. :wink:
+// In oltre per scorrere le immagini utilizziamo anche le frecce sinistra e destra della tastiera.
