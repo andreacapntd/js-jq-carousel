454915d40374c2f178a7657ed5e833252baa9556
andreacapntd <andreacapntd@gmail.com>
2020-07-03 17:07:25 +0200

    ex js jq add img next function

--- main.js
+++ main.js
@@ -3,3 +3,25 @@
 // Per facilitarvi la vita usate immagini delle stesse dimensioni :slightly_smiling_face:
 // Lo slider prevederà due frecce (icone) che permettono di mostrare le immagini, arrivati all’ultima o prima immagine dobbiamo ripartire dal lato opposto. :wink:
 // In oltre per scorrere le immagini utilizziamo anche le frecce sinistra e destra della tastiera.
+
+
+$(document).ready(function (){
+  var imgNext = $(".next");
+  var imgPrev = $(".prev");
+
+  imgNext.click(function () {
+    var imgActive = $(".slider-wrapper img.active");
+    imgActive.removeClass("active");
+    var iActive = $(".slider-wrapper i.active");
+    iActive.removeClass("active");
+    if (imgActive.hasClass("last")) {
+      $(".slider-wrapper img.first").addClass("active");
+      $(".slider-wrapper i.first").addClass("active");
+    } else {
+      imgActive.next().addClass("active");
+      iActive.next().addClass("active");
+
+    }
+  });
+});
+//-------------------Functions-----------------------
